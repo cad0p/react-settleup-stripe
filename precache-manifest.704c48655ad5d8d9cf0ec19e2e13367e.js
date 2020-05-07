45f8bced75e8928df3f6b45a93a481513f135852
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2020-05-07 05:22:51 +0200

    Updates

---
+++ precache-manifest.704c48655ad5d8d9cf0ec19e2e13367e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa92577cc855bab78a812af0baea9bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97725387f28d11c9305f",
+    "url": "/static/js/2.6fe13137.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.6fe13137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1365cf0ed965131028",
+    "url": "/static/js/main.30432023.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
